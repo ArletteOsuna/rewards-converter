672047b636d2b8f4c58f007d1ab2427614fa29cd
arletteosuna <arletteosuna101@gmail.com>
2024-06-20 17:26:54 -0700

    Update RewardValue.java with constructors for cash and miles

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -3,13 +3,13 @@ public class RewardValue {
         private double milesValue;
         private static final double MILES_TO_CASH_RATE = 0.0035;
 
-        // Constructor that accepts a cash value
+        // Accepts a cash value
         public RewardValue(double cashValue) {
             this.cashValue = cashValue;
             this.milesValue = cashValue / MILES_TO_CASH_RATE;
         }
 
-        // Constructor that accepts a miles value
+        // Accepts a miles value
         public RewardValue(int milesValue) {
             this.milesValue = milesValue;
             this.cashValue = milesValue * MILES_TO_CASH_RATE;
